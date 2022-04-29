a3522d856b8341fac0a79f2311099095937ff86e
Charles Haussman <cbh225@lehigh.edu>
2022-04-28 23:41:26 -0400

    HW 5

---
+++ HW 5.R
@@ -0,0 +1,23 @@
+library("tidyverse")
+library("ggplot2")
+
+df <- read.csv("TBQ05.20211116133909.csv")
+
+view(df)
+summary(df)
+
+df$VALUE <- as.numeric(df$VALUE)
+df$VALUE <- replace(df$VALUE, is.na(df$VALUE),0)
+
+df$Region.of.Trade <- as.factor(df$Region.of.Trade)
+by(df$VALUE, df$Region.of.Trade, sum)
+
+df <- df[which(df$Port!="ALl Main Irish Ports")]
+
+ggplot(data = df) +
+  geom_point(aes(x=Quarter, y=VALUE, color=Port)) +
+  ggtitle("Quarterly Tonnage Arriving from Great Britain and Northern Ireland") +
+  xlab("Quarter") +
+  ylab("Tonnage") +
+  ylim(0, 4000) +
+  theme_minimal()
